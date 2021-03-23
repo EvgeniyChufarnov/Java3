62c9ff30d1d918eaa30cd4c0e21174caae14de2f
Evgeniy Chufarnov <evgeniychufarnov@gmail.com>
2021-03-23 23:32:25 +0300

    homework #4 is done

--- server/src/main/java/com/geekbrains/server/ClientHandler.java
+++ server/src/main/java/com/geekbrains/server/ClientHandler.java
@@ -5,69 +5,88 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class ClientHandler {
+public class ClientHandler implements Runnable {
     private String nickname;
     private Server server;
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private final DataBaseSingleton dataBaseSingleton;
+
+    private Boolean isAuthorised = false;
+    private Boolean isStopped = false;
 
     public String getNickname() {
         return nickname;
     }
     public ClientHandler(Server server, Socket socket, DataBaseSingleton dataBaseSingleton) {
+        this.dataBaseSingleton = dataBaseSingleton;
+        init(server, socket);
+    }
+
+    private void init(Server server, Socket socket) {
         try {
             this.server = server;
             this.socket = socket;
             this.in = new DataInputStream(socket.getInputStream());
             this.out = new DataOutputStream(socket.getOutputStream());
-            new Thread(() -> {
-                try {
-                    while (true) {
-                        String msg = in.readUTF();
-                        // /auth login1 pass1
-                        if (msg.startsWith("/auth ")) {
-                            String[] tokens = msg.split("\\s");
-                            String nick = dataBaseSingleton.getNicknameOrNull(tokens[1], tokens[2]);
-                            if (nick != null && !server.isNickBusy(nick)) {
-                                sendMsg("/authok " + nick);
-                                nickname = nick;
-                                server.subscribe(this);
-                                break;
-                            }
-                        }
-                    }
-                    while (true) {
-                        String msg = in.readUTF();
-                        if(msg.startsWith("/")) {
-                            if (msg.equals("/end")) {
-                                sendMsg("/end");
-                                break;
-                            }
-                            if(msg.startsWith("/w ")) {
-                                String[] tokens = msg.split("\\s", 3);
-                                server.privateMsg(this, tokens[1], tokens[2]);
-                            }
-                            if(msg.startsWith("/change ")) {
-                                String[] tokens = msg.split("\\s", 3);
-                                boolean success = dataBaseSingleton.updateNickname(nickname, tokens[1]);
-                                if (success) {
-                                    server.broadcastMsg("user " + nickname + " changed nickname to " + tokens[1]);
-                                    nickname = tokens[1];
-                                }
-                            }
-                        } else {
-                            server.broadcastMsg(nickname + ": " + msg);
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    ClientHandler.this.disconnect();
-                }
-            }).start();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!isAuthorised) {
+                tryToAuthorize();
+            }
+            while (!isStopped) {
+                receiveMessage();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            ClientHandler.this.disconnect();
+        }
+    }
+
+    private void tryToAuthorize() throws IOException {
+        String msg = in.readUTF();
+        // /auth login1 pass1
+        if (msg.startsWith("/auth ")) {
+            String[] tokens = msg.split("\\s");
+            String nick = dataBaseSingleton.getNicknameOrNull(tokens[1], tokens[2]);
+            if (nick != null && !server.isNickBusy(nick)) {
+                sendMsg("/authok " + nick);
+                nickname = nick;
+                server.subscribe(this);
+                isAuthorised = true;
+            }
+        }
+    }
+
+    private void receiveMessage() throws IOException {
+        String msg = in.readUTF();
+        if(msg.startsWith("/")) {
+            if (msg.equals("/end")) {
+                sendMsg("/end");
+                isStopped = true;
+            }
+            if(msg.startsWith("/w ")) {
+                String[] tokens = msg.split("\\s", 3);
+                server.privateMsg(this, tokens[1], tokens[2]);
+            }
+            if(msg.startsWith("/change ")) {
+                String[] tokens = msg.split("\\s", 3);
+                boolean success = dataBaseSingleton.updateNickname(nickname, tokens[1]);
+                if (success) {
+                    server.broadcastMsg("user " + nickname + " changed nickname to " + tokens[1]);
+                    nickname = tokens[1];
+                }
+            }
+        } else {
+            server.broadcastMsg(nickname + ": " + msg);
         }
     }
 
